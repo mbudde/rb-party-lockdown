a8cdcb4465ed3326da5806cbdb392075f6b1c5e1
Michael Budde <mbudde@gmail.com>
2008-11-08 23:05:20 +0100

    Add missing imports in Dialogs.py

--- src/Dialogs.py
+++ src/Dialogs.py
@@ -16,6 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.
 
+import gobject, gtk
+
 class PreferenceDialog(object):
     def __init__(self, plugin):
         self.plugin = plugin
